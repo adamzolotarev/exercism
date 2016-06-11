b16d7490180780a1032b7252fab9d1914fd3d218
Adam Zolotarev <eugenezolo@gmail.com>
2016-06-11 18:13:19 -0400

    Added word count

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,30 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map
+  def count(sentence) do
+      sentence
+        |> clean_input
+        |> String.split
+        |> count_words
+  end
+
+  def count_words(words) when is_list(words) do
+    Enum.reduce(words, %{}, &update_count/2)
+    # Enum.reduce(words, %{}, &update_count/2)
+  end
+
+  defp clean_input(input) do
+    input
+      |> String.replace(~r/[.,\/#!$%\^&@\*;:{}=\`~()]/, "")
+      |> String.replace("_", " ")
+      |> String.downcase
+  end
+  
+  defp update_count(word, acc) do
+    Map.update acc, word, 1, &(&1 + 1)
+  end
+end
