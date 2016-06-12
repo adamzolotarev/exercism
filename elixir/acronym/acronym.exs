72c7820ea7a652f2be02a7247ae40ab66d76db6a
Adam Zolotarev <eugenezolo@gmail.com>
2016-06-12 14:00:35 -0400

    Updated to use Regex.scan

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,20 +5,8 @@ defmodule Acronym do
   """
   @spec abbreviate(string) :: String.t()
   def abbreviate(string) do
-    string
-    |> String.split
-    |> Enum.map(fn(x) -> capitalizeFirstLetter(x) end)
+    Regex.scan(~r/(\p{Lu}|\b\w)/u, string, capture: :first)
     |> Enum.join
-    |> String.graphemes
-    |> Enum.filter(fn(x) -> String.match?(x, ~r/^\p{Lu}$/u) end)
-    |> Enum.join
+    |> String.upcase
   end
-
-  defp capitalizeFirstLetter(word) do
-    firstLetter = word
-      |> String.slice(0, 1)
-      |> String.capitalize()
-    firstLetter <> String.slice(word, 1..-1)
-  end
-
 end
