fb764a9799579023394cc22c42564e4df7810f45
Adam Zolotarev <eugenezolo@gmail.com>
2016-07-12 17:57:37 -0400

    Use when clause instead of case in Hamming

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -8,12 +8,11 @@ defmodule DNA do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(strand1, strand2) do
-  	case length(strand1) == length(strand2) do
-  		false -> {:error, "Lists must be the same length."}
-  		true -> count = {:ok, _hamming_distance(strand1, strand2, 0)}
-  	end
+  def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do
+  	{:ok, _hamming_distance(strand1, strand2, 0)}
   end
+
+ def hamming_distance(_, _), do: {:error, "Lists must be the same length."}
 
   defp _hamming_distance(h, h, acc), do: acc
   defp _hamming_distance([h|t1], [h|t2], acc), do: _hamming_distance(t1, t2, acc)
