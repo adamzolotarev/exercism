c0fb0b262cdf5a806e03f02a9c82382fc679556e
Adam Zolotarev <eugenezolo@gmail.com>
2016-09-26 18:21:06 -0400

    Update Gigasecond implementation to use a module

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,11 @@
-class Gigasecond
-  def self.from(utc_time)
-    utc_time + 1_000_000_000
+module Gigasecond
+  GIGASECOND = 1_000_000_000
+  def self.from(seconds)
+    seconds + GIGASECOND
   end
 end
+
+Gigasecond.extend(Gigasecond)
 
 module BookKeeping
   VERSION = 4
