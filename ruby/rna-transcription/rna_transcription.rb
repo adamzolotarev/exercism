32e133db10d1a09ee6c79a73aa3d4bc7514dff24
Adam Zolotarev <eugenezolo@gmail.com>
2016-09-26 19:38:43 -0400

    Use direct hash access instead of fetch

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -10,7 +10,7 @@ module Complement
     return '' unless valid? dna_strand
 
     rna = ''
-    dna_strand.each_char { |c| rna += @dna_to_rna_map.fetch(c, '') }
+    dna_strand.each_char { |c| rna += @dna_to_rna_map[c] }
     rna
   end
 
