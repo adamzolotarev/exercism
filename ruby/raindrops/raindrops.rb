c0d194a9ab7e3c2ea6a1da1ad2a80d348914ae77
Adam Zolotarev <eugenezolo@gmail.com>
2016-09-27 17:39:39 -0400

    Use empty?

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,14 +1,12 @@
 module Raindrops
-	def convert(number)
-		drop = ''
-		drop = 'Pling' if number % 3 == 0
-		drop += 'Plang' if number % 5 == 0
-		drop += 'Plong' if number % 7 == 0
+  def convert(number)
+    drop = ''
+    drop = 'Pling' if number % 3 == 0
+    drop += 'Plang' if number % 5 == 0
+    drop += 'Plong' if number % 7 == 0
 
-		return number.to_s if drop == ''
-
-		drop
-	end
+    drop.empty? ? number.to_s : drop
+  end
 end
 
 Raindrops.extend(Raindrops)
