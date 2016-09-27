30133f24846151fbd91d33e47371e735f2b1d87d
Adam Zolotarev <eugenezolo@gmail.com>
2016-09-27 17:28:23 -0400

    Raindrops implementatino

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,18 @@
+module Raindrops
+	def convert(number)
+		drop = ''
+		drop = 'Pling' if number % 3 == 0
+		drop += 'Plang' if number % 5 == 0
+		drop += 'Plong' if number % 7 == 0
+
+		return number.to_s if drop == ''
+
+		drop
+	end
+end
+
+Raindrops.extend(Raindrops)
+
+module BookKeeping
+  VERSION = 2
+end
