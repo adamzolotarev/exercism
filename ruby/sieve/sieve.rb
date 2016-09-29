97f04cdccbd6a5e52130291821648c5fafedf007
Adam Zolotarev <eugenezolo@gmail.com>
2016-09-29 18:28:48 -0400

    Use reject for Sieve instead of each

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,12 +1,12 @@
 class Sieve
   def initialize(number)
-    @number = number
+    @range = (2..number)
   end
 
   def primes
-    primes = (2..@number).to_a
-    (2..@number).each do |n|
-      primes.each { |p| primes.delete(p) if (p % n).zero? && p != n }
+    primes = @range.to_a
+    @range.each do |n|
+      primes = primes.reject { |p| (p % n).zero? && p != n }
     end
     primes
   end
