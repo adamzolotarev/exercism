032be9e73eea6f41f28a298b868b8b648b90c097
Adam Zolotarev <eugenezolo@gmail.com>
2016-09-30 22:41:22 -0400

    Replace if's with hash

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,25 +1,30 @@
-class Numeric
+class Integer
+  ROMAN_NUMERALS =
+    {
+      1000 => 'M',
+      900 => 'CM',
+      500 => 'D',
+      400 => 'CD',
+      100 => 'C',
+      90 => 'XC',
+      50 => 'L',
+      40 => 'XL',
+      10 => 'X',
+      9 => 'IX',
+      5 => 'V',
+      4 => 'IV',
+      1 => 'I'
+    }.freeze
+
   def to_roman
-    return 'I' if self == 1
-    return 'I' * self if self < 4
-    return 'I' + 5.to_roman if self < 5
-    return 'V' if self == 5
-    return 'V' + (self - 5).to_roman if self < 9
-    return 'IX' if self == 9
-    return 'X' if self == 10
-    return 'X' * (self / 10) + (self % 10).to_roman if self < 40
-    return 'XL' + (self % 10).to_roman if self < 50
-    return 'L' if self == 50
-    return 'L' + (self % 50).to_roman if self < 90
-    return 'XC' + (self % 10).to_roman if self < 100
-    return 'C' if self == 100
-    return 'C' * (self / 100) + (self % 100).to_roman if self < 400
-    return 'CD' + (self % 400).to_roman if self < 500
-    return 'D' if self == 500
-    return 'D' + (self % 500).to_roman if self < 900
-    return 'CM' + (self % 900).to_roman if self < 1000
-    return 'M' if self == 1000
-    return 'M' * (self / 1000) + (self % 1000).to_roman if self < 4000
+    number = self
+    roman_conversion = ''
+
+    ROMAN_NUMERALS.each do |normal, roman|
+      roman_conversion << roman * (number / normal)
+      number = number % normal
+    end
+    roman_conversion
   end
 end
 
