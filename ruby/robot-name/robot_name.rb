1d14349390b6553f78d40cd6bc877b3878c34293
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-12 18:03:05 -0400

    Remove threads; add private attributes for robot_name

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,23 +1,26 @@
 require 'set'
 
 class Robot
-  Thread.current[:taken_robot_names] ||= Set.new
+  attr_reader :name
+  @@taken_robot_names ||= Set.new
+
   def initialize
     @name = random_name
   end
 
   def random_name
     new_name = Array.new(2) { ('A'..'Z').to_a.sample }.join +
-               Array.new(3) { ('0'..'9').to_a.sample }.join
-    return new_name if Thread.current[:taken_robot_names].add?(new_name)
+      Array.new(3) { ('0'..'9').to_a.sample }.join
+    return new_name if @@taken_robot_names.add?(new_name)
     random_name
   end
 
   def reset
     @name = random_name
+    @@taken_robot_names.delete?(@name)
   end
 
-  attr_accessor :name
+  private :random_name
 end
 
 module BookKeeping
