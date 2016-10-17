7df3c96ba05fc58ab34dc7f9cc916ff74519e8cf
Adam Zolotarev <eugenezolo@gmail.com>
2016-10-17 17:43:19 -0400

    Leap year implementation

---
+++ ruby/leap/leap.rb
@@ -0,0 +1,12 @@
+class Year
+  def self.leap?(year)
+    return true if (year % 400).zero?
+    return false if (year % 100).zero?
+    return true if (year % 4).zero?
+    false
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
