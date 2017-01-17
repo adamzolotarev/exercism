3345e7148a1f520c851a73e98f7c5d4e8dade962
Adam Zolotarev <eugenezolo@gmail>
2017-01-17 18:07:31 -0500

    Add beer song ruby implementation

---
+++ ruby/beer-song/beer_song.rb
@@ -0,0 +1,48 @@
+class BeerSong
+
+  def verse(verse_number)
+    first_line(verse_number) + second_line(verse_number)
+  end
+
+  def verses(verse_number_high_bound, verse_number_low_bound)
+    lyrics = ''
+    verse_number_high_bound.downto(verse_number_low_bound).each do |verse_number|
+      lyrics += verse(verse_number)
+      lyrics += "\n" if verse_number != verse_number_low_bound
+    end
+    lyrics
+  end
+
+  def lyrics
+    verses(99, 0)
+  end
+
+  private
+
+  def first_line(verse_number)
+    return "No more bottles of beer on the wall, no more bottles of beer.\n"\
+           if verse_number.zero?
+
+    bottles = bottle_term(verse_number)
+    "#{verse_number} #{bottles} of beer on the wall, #{verse_number} #{bottles} of beer.\n"
+  end
+
+  def second_line(verse_number)
+    return "Take it down and pass it around, no more bottles of beer on the wall.\n"\
+          if verse_number == 1
+    return "Go to the store and buy some more, 99 bottles of beer on the wall.\n"\
+          if verse_number.zero?
+
+    bottles = bottle_term(verse_number - 1)
+    "Take one down and pass it around, #{verse_number - 1} #{bottles} of beer on the wall.\n"
+  end
+
+  def bottle_term(bottle_term)
+    return 'bottle' if bottle_term == 1
+    'bottles'
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
