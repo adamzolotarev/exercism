f131c8661507993610513d0ed95b6fcb49dd7a29
Adam Zolotarev <eugenezolo@gmail>
2017-02-21 18:19:42 -0500

    Refactor to use define_method

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -2,47 +2,25 @@ class SpaceAge
   attr_reader :seconds
 
   EARTH_YEAR_SECONDS = 31_557_600.0
-  MERCURY_YEAR_SECONDS = 0.2408467 * EARTH_YEAR_SECONDS
-  VENUS_YEAR_SECONDS = 0.61519726 * EARTH_YEAR_SECONDS
-  MARS_YEAR_SECONDS = 1.8808158 * EARTH_YEAR_SECONDS
-  JUPITER_YEAR_SECONDS = 11.862615 * EARTH_YEAR_SECONDS
-  SATURN_YEAR_SECONDS = 29.447498 * EARTH_YEAR_SECONDS
-  URANUS_YEAR_SECONDS = 84.016846 * EARTH_YEAR_SECONDS
-  NEPTUNE_YEAR_SECONDS = 164.79132 * EARTH_YEAR_SECONDS
 
   def initialize(seconds)
     @seconds = seconds
   end
 
-  def on_earth
-    seconds / EARTH_YEAR_SECONDS
-  end
+  PLANETS_ORBITAL_PERIOD = {
+    'earth' => 1.0,
+    'mercury' => 0.2408467,
+    'venus' => 0.61519726,
+    'mars' => 1.8808158,
+    'jupiter' => 11.862615,
+    'saturn' =>  29.447498,
+    'uranus' =>  84.016846,
+    'neptune' => 164.79132
+  }.freeze
 
-  def on_mercury
-    seconds / MERCURY_YEAR_SECONDS
-  end
-
-  def on_venus
-    seconds / VENUS_YEAR_SECONDS
-  end
-
-  def on_mars
-    seconds / MARS_YEAR_SECONDS
-  end
-
-  def on_jupiter
-    seconds / JUPITER_YEAR_SECONDS
-  end
-
-  def on_saturn
-    seconds / SATURN_YEAR_SECONDS
-  end
-
-  def on_uranus
-    seconds / URANUS_YEAR_SECONDS
-  end
-
-  def on_neptune
-    seconds / NEPTUNE_YEAR_SECONDS
+  PLANETS_ORBITAL_PERIOD.each do |planet, earth_years|
+    define_method :"on_#{planet}" do
+      seconds / (EARTH_YEAR_SECONDS * earth_years)
+    end
   end
 end
