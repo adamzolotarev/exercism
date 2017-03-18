5af768e506f4952db6bb7ef7f594d7bc747e17d3
Adam Zolotarev <eugenezolo@gmail>
2017-03-18 19:26:01 -0400

    Refactor rotational cipher

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -1,4 +1,8 @@
 defmodule RotationalCipher do
+
+  @upper_case ?A..?Z
+  @lower_case ?a..?z
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,8 +12,6 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
-    # alpahbet_shift = rem(char + alphabet_shift, (97 - 65))
-
     text
     |> to_charlist
     |> Enum.map(&rotate_char(&1, shift))
@@ -17,31 +19,13 @@ defmodule RotationalCipher do
   end
 
   defp rotate_char(char, shift) do
+    rotate = fn (start, char) -> start + rem(char - start + shift, 26) end
+
     cond do
-      is_upper_case_letter(char) -> rotate_upper_letter(char, shift)
-      is_lower_case_letter(char) -> rotate_lower_letter(char, shift)
+      char in @upper_case -> rotate.(?A, char)
+      char in @lower_case -> rotate.(?a, char)
       true -> char
     end
-  end
-
-  defp is_upper_case_letter(char) do
-    char >= 65 && char <= 90
-  end
-
-  defp is_lower_case_letter(char) do
-    char >= 97 && char <= 122
-  end
-
-  defp rotate_upper_letter(char, shift) do
-    alphabet_shift = rem(shift, 26)
-    capital_a_code = 65
-    capital_a_code + rem(char - capital_a_code + alphabet_shift, 26)
-  end
-
-  defp rotate_lower_letter(char, shift) do
-    alphabet_shift = rem(shift, 26)
-    lower_a_code = 97
-    lower_a_code + rem(char - lower_a_code + alphabet_shift, 26)
   end
 
 end
--- elixir/rotational-cipher/rotational_cipher_test.exs
+++ elixir/rotational-cipher/rotational_cipher_test.exs
@@ -8,7 +8,7 @@ ExUnit.configure exclude: :pending, trace: true
 defmodule RotationalCipherTest do
   use ExUnit.Case
 
-  #@tag :pending
+  # @tag :pending
   test "rotate a by 1" do
     plaintext = "a"
     shift = 1
