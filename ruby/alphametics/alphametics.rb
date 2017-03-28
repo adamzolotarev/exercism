d84007b7ea69c1756f6f6d35e315f22611406263
Adam Zolotarev <eugenezolo@gmail>
2017-03-28 17:59:20 -0400

    Add ruby alphametics implementation

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,9 +1,8 @@
 class Alphametics
   def self.solve(equation)
-    # equation.gsub!('^', '**')
     letters = equation.scan(/[A-Z]/).uniq
-
-    (0..9).to_a.permutation(letters.count).each do |digits|
+    permutations = (0..9).to_a.permutation(letters.count)
+    permutations.each do |digits|
       map = Hash[*letters.zip(digits).flatten]
 
       substitued = equation.gsub(/[A-Z]/, map)
@@ -11,6 +10,7 @@ class Alphametics
 
       return map if eval(substitued)
     end
+    {}
   end
 end
 
